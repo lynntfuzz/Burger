882c35df6f5b5c0e7f8a100ec79c0b3b3b2ed32e
Lynn Thompson <lynninla@earthlink.net>
2018-12-11 21:25:44 -0800

    First commit of app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rocket Burger with cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Monkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Happy Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lynn Burger', false);
+INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
